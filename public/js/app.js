376eb6d0418bb22b956fe2cac15e0f3af0d1b988
Karthik Balakrishnan <karthikb351@gmail.com>
2015-04-03 17:38:27 +0530

    Moved to bootstrap
     - Superhero theme
     - Testing mailto: link for mobile

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,7 @@
 $("#answers1").dragend();
-$("#answers2").dragend();
+
+$('#emailForm').submit( function(e){ 
+      e.preventDefault();
+      var email = $('#emailId').value;
+      $(this).attr('target', 'mailto:'+email);
+    });
