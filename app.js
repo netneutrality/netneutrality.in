c7f613381e9db84698ed602f30ed3089076fe216
Karthik Balakrishnan <karthikb351@gmail.com>
2015-12-22 19:50:25 +0530

    redirect to sti

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get("/", function (req, res) {
+  res.redirect(307, "http://www.savetheinternet.in");
+});
+
 app.use(express.static(__dirname + '/src'));
 
 app.listen(process.env.PORT || 4432);
